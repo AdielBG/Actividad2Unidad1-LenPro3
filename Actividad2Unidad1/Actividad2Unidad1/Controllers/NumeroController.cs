d8098977e03b5090baa399d829db201964766522
AdielBG <adielbg17.2@gmail.com>
2025-05-25 19:48:45 -0400

    TercerEjercicio

---
+++ Actividad2Unidad1/Actividad2Unidad1/Controllers/NumeroController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Actividad2Unidad1.Controllers
+{
+    public class NumeroController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Verificar(int numero)
+        {
+            string mensaje;
+
+            if (numero <= 1)
+            {
+                mensaje = "Debe ingresar un número mayor que 1.";
+            }
+            else
+            {
+                bool primo = true;
+                for (int i = 2; i < numero; i++)
+                {
+                    if (numero % i == 0)
+                    {
+                        primo = false;
+                        break;
+                    }
+                }
+
+                mensaje = primo
+                    ? $"{numero} es un número primo."
+                    : $"{numero} no es un número primo.";
+            }
+
+            ViewBag.Mensaje = mensaje;
+            return View();
+
+        }
+    }
+
+}
--- Actividad2Unidad1/Actividad2Unidad1/obj/Debug/net8.0/Actividad2Unidad1.AssemblyInfo.cs
+++ Actividad2Unidad1/Actividad2Unidad1/obj/Debug/net8.0/Actividad2Unidad1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Actividad2Unidad1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4a9de1090c3bda51bb1d094b791a01e35296699")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89bf17157fb479089db7ddfad5c6017d76151a6d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Actividad2Unidad1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Actividad2Unidad1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
