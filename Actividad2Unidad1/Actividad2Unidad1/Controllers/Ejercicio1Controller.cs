d4a9de1090c3bda51bb1d094b791a01e35296699
AdielBG <adielbg17.2@gmail.com>
2025-05-25 17:09:50 -0400

    PrimerEjercicio

---
+++ Actividad2Unidad1/Actividad2Unidad1/Controllers/Ejercicio1Controller.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Actividad2Unidad1.Controllers
+{
+    public class Ejercicio1Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Saludar(string nombre)
+        {
+            if (!string.IsNullOrWhiteSpace(nombre))
+            {
+                ViewBag.Mensaje = $"Hola, {nombre}, bienvenido a ASP.NET MVC.";
+            }
+            else
+            {
+                ViewBag.Mensaje = $"Hola extraño, tienes que ingresar tu nombre para ver la bienvenida.";
+            }
+
+            return View();
+        }
+    }
+}
